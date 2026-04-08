a08bc0907daae4710dc893a523e90c34792e8112
Eirren <edv2019@gmail.com>
2026-04-08 13:45:33 -0700

    Milestone 1, added a list clear

--- main.cpp
+++ main.cpp
@@ -47,7 +47,7 @@ int main() {
     
     // Goat Manager 3001 Engine
     int sel = main_menu();
-    while (sel != 4) {
+    while (sel != 5) {
         switch (sel) {
             case 1:
                 cout << "Adding a goat.\n";
@@ -60,6 +60,11 @@ int main() {
             case 3:    
                 cout << "Displaying goat data.\n";
                 display_trip(trip);
+                break;
+            case 4:
+                // first milestone, clear list
+                cout << "Clearing list.\n";
+                trip.clear();
                 break;
             default:
                 cout << "Invalid selection.\n";
@@ -77,11 +82,12 @@ int main_menu() {
     cout << "[1] Add a goat\n";
     cout << "[2] Delete a goat\n";
     cout << "[3] List goats\n";
-    cout << "[4] Quit\n";
+    cout << "[4] Clear goats from list\n";
+    cout << "[5] Quit\n";
     cout << "Choice --> ";
     int choice;
     cin >> choice;
-    while (choice < 1 || choice > 4) {
+    while (choice < 1 || choice > 5) {
         cout << "Invalid, again --> ";
         cin >> choice;
     }
